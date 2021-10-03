7b835576401c6fa1b7b67d3a1e15146cec4e1de5
tam@gmail.com <tam@gmail.com>
2021-10-03 22:33:46 +0200

    update $policies in AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,9 +3,17 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
+use App\Models\Role;
+use App\Models\Tag;
+use App\Models\Video;
 use App\Policies\CategoryPolicy;
+use App\Policies\CommentPolicy;
 use App\Policies\PostPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\TagPolicy;
+use App\Policies\VideoPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,19 +24,16 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        Tag::class => TagPolicy::class,
         Post::class => PostPolicy::class,
+        Role::class => RolePolicy::class,
+        Video::class => VideoPolicy::class,
+        Comment::class => CommentPolicy::class,
         Category::class => CategoryPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
