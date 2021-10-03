bd8d39bfa653ffc24d1a783711cbfdf327e8edde
tam@gmail.com <tam@gmail.com>
2021-10-03 22:25:18 +0200

    added CommentSeeder.php

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use Illuminate\Database\Seeder;
+
+class CommentSeeder extends Seeder
+{
+
+    public function run()
+    {
+        Comment::factory()
+            ->count(5)
+            ->create();
+    }
+}
