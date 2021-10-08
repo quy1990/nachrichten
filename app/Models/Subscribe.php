bb3a03e60053513f8ae95e7a157f7f5c8c2840a1
tam@gmail.com <tam@gmail.com>
2021-10-08 21:09:15 +0200

    adjusted subscribe just for Category, Post, Video, User

--- app/Models/Subscribe.php
+++ app/Models/Subscribe.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Subscribe extends Model
 {
@@ -14,14 +12,4 @@ class Subscribe extends Model
     public $timestamps = false;
 
     protected $fillable = ['user_id', 'subscribable_id', 'subscribable_type'];
-
-    public function subscribable(): MorphTo
-    {
-        return $this->morphTo();
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
 }
