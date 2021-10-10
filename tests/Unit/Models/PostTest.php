eec1b750cdeef13947030a51a6ac8631215a51dc
tam@gmail.com <tam@gmail.com>
2021-10-10 22:18:19 +0200

    adjusted for testcase

--- tests/Unit/Models/PostTest.php
+++ tests/Unit/Models/PostTest.php
@@ -3,13 +3,12 @@
 namespace Models;
 
 use App\Models\Category;
-use App\Models\Comment;
 use App\Models\Image;
 use App\Models\Post;
-use App\Models\Role;
 use App\Models\Subscribable;
+use App\Models\Tag;
+use App\Models\Taggable;
 use App\Models\User;
-use App\Models\Video;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -20,62 +19,65 @@ class PostTest extends TestCase
     private $testedUser;
     private $users;
     private $categories;
-    private $videos;
     private $posts;
-    private $role;
-    private $comment;
-    private $subscribedUsers;
     private $subscribedPosts;
-    private $subscribedCategories;
-    private $subscribedVideos;
     private $image;
+    private $taggables;
+    private $tag;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->role = Role::factory()->create();
         $this->testedUser = User::factory()->create();
-        $this->testedUser->roles()->attach($this->role->id);
         $this->categories = Category::factory(env('TEST_COUNT'))->create();
         $this->users = User::factory(env('TEST_COUNT'))->create();
-        $this->videos = Video::factory(env('TEST_COUNT'))->create(['user_id' => $this->testedUser->id, 'category_id' => $this->categories[0]->id]);
         $this->posts = Post::factory(env('TEST_COUNT') * 2)->create(['user_id' => $this->testedUser->id, 'category_id' => $this->categories[0]->id]);
-        $this->comment = Comment::factory()->create(['user_id' => $this->testedUser->id]);
-        $this->image = Image::factory()->create(['imageable_id' => $this->testedUser->id, 'imageable_type' => 'App\Models\User']);
+        $this->image = Image::factory()->create(['imageable_id' => $this->categories[0]->id, 'imageable_type' => 'App\Models\Category']);
+        $this->tag = Tag::factory()->create();
 
-        foreach ($this->users as $user) {
-            $this->subscribedUsers[] = Subscribable::factory()->create(['user_id' => $this->testedUser->id, 'subscribable_id' => $user->id, 'subscribable_type' => 'App\Models\User']);
-        }
         foreach ($this->posts as $post) {
             $this->subscribedPosts[] = Subscribable::factory()->create(['user_id' => $this->testedUser->id, 'subscribable_id' => $post->id, 'subscribable_type' => 'App\Models\Post']);
         }
-        foreach ($this->categories as $category) {
-            $this->subscribedCategories[] = Subscribable::factory()->create(['user_id' => $this->testedUser->id, 'subscribable_id' => $category->id, 'subscribable_type' => 'App\Models\Category']);
+
+        foreach ($this->posts as $post) {
+            $this->taggables[] = Taggable::factory()->create(['tag_id' => $this->tag->id, 'taggable_id' => $post->id, 'taggable_type' => 'App\Models\Post']);
         }
-        foreach ($this->videos as $video) {
-            $this->subscribedVideos[] = Subscribable::factory()->create(['user_id' => $this->testedUser->id, 'subscribable_id' => $video->id, 'subscribable_type' => 'App\Models\Video']);
+    }
+
+
+    public function testUser()
+    {
+        $user = Post::where('user_id', $this->testedUser->id)->first()->user;
+        self::assertEquals($user->getAttributes(), $this->testedUser->getAttributes());
+    }
+
+    public function testCategory()
+    {
+        $category = Post::where('user_id', $this->testedUser->id)->first()->category;
+        self::assertEquals($category->getAttributes(), $this->categories[0]->getAttributes());
+    }
+
+    public function testImage()
+    {
+        $image = Image::where('imageable_id', $this->categories[0]->id)
+            ->where('imageable_type', 'App\Models\Category')
+            ->first();
+        self::assertEquals($image->getAttributes(), $this->image->getAttributes());
+    }
+
+    public function testTags()
+    {
+        $tags = Post::find($this->posts[0]->id)->tags;
+        foreach ($tags as $item) {
+            $item = $item->getAttributes();
+            self::assertEquals($item, $this->tag->getAttributes());
         }
     }
 
     public function testSubscribers()
     {
-        $subscribedUsers = Subscribable::select(['user_id', 'subscribable_id', 'subscribable_type'])->where('user_id', $this->testedUser->id)
-            ->where('subscribable_type', 'App\Models\User')->get()->toArray();
-        foreach ($this->subscribedUsers as $item) {
-            $item = $item->getAttributes('user_id', 'subscribable_id', 'subscribable_type');
-            unset($item['id']);
-            self::assertTrue(in_array($item, $subscribedUsers));
-        }
-    }
-
-    public function testSubscribedPosts()
-    {
-        $subscribedCategories = Subscribable::select(['user_id', 'subscribable_id', 'subscribable_type'])->where('user_id', $this->testedUser->id)
-            ->where('subscribable_type', 'App\Models\Category')->get()->toArray();
-        foreach ($this->subscribedCategories as $item) {
-            $item = $item->getAttributes('user_id', 'subscribable_id', 'subscribable_type');
-            unset($item['id']);
-            self::assertTrue(in_array($item, $subscribedCategories));
+        foreach ($this->posts as $post) {
+            self::assertEquals($post->subscribers->first()->getAttributes(), $this->testedUser->getAttributes());
         }
     }
 
