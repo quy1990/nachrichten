ff1acc1e0916cd9bc4662e1999e94db163488c2e
tam@gmail.com <tam@gmail.com>
2021-10-15 21:45:31 +0200

    fixed error

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Video extends Model
@@ -13,6 +14,16 @@ class Video extends Model
     public $timestamps = false;
 
     protected $fillable = ['name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 
     public function tags(): MorphToMany
     {
