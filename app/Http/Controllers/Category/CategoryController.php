8b237fd4fab1c71cbd6d6262830ffa9371ce79a6
tam@gmail.com <tam@gmail.com>
2021-11-17 16:27:55 +0100

    Add requests in CategoryController.php

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Categories\CategoryStoreRequest;
+use App\Http\Requests\Categories\CategoryUpdateRequest;
 use App\Http\Resources\Category\CategoryCollection;
 use App\Http\Resources\Category\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
 use Lukasoppermann\Httpstatus\Httpstatuscodes as Status;
 
 class CategoryController extends Controller
@@ -21,7 +25,7 @@ class CategoryController extends Controller
         return new CategoryCollection(Category::paginate(20));
     }
 
-    public function store(Request $request): CategoryResource
+    public function store(CategoryStoreRequest $request): CategoryResource
     {
         $category = new Category();
         $category->name = $request->get('name');
@@ -34,14 +38,14 @@ class CategoryController extends Controller
         return new CategoryResource($category);
     }
 
-    public function update(Request $request, Category $category): CategoryResource
+    public function update(CategoryUpdateRequest $request, Category $category): CategoryResource
     {
         $category->name = $request->get('name');
         $category->save();
         return new CategoryResource($category);
     }
 
-    public function destroy(Category $category)
+    public function destroy(Category $category): Response|Application|ResponseFactory
     {
         $category->delete();
         return response("", Status::HTTP_NO_CONTENT);
