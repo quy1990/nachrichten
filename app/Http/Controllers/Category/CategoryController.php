f467aed7ab929fe51dc16314a0438551a6a1f6c4
anioparasicase <anioparasicase@gmail.com>
2022-04-29 22:04:29 +0700

    change conttoller

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -18,7 +18,7 @@ class CategoryController extends Controller
     public function __construct()
     {
         //$this->authorizeResource(Category::class, 'category');
-
+//    intlcal_is_equivalent_to();
 
     }
 
